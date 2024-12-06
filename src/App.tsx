a429cea8ff44c65de256d4c8285369aa6fd032c8
Danyil <68147415+kdv1995@users.noreply.github.com>
2024-12-06 15:30:55 +0200

    udpate

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { Router } from "./router/main";
 
 function App() {
+  const { DEV, PROD, SSR, MODE } = import.meta.env;
+  console.log({
+    DEV,
+    PROD,
+    SSR,
+    MODE,
+    CUSTOM_DEV: import.meta.env.VITE_API_URL_DEV,
+    CUSTOM_PROD: import.meta.env.VITE_API_URL_PROD,
+    CUSTOME_MODE_DEV: import.meta.env.VITE_MODE_DEV,
+  });
   return <Router />;
 }
 
