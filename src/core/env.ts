c161cdc8252493d22e845e9ff3fe868fbfca895f
Danyil <68147415+kdv1995@users.noreply.github.com>
2024-12-06 15:52:31 +0200

    update

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,3 +1,3 @@
 export const env = {
-  api: JSON.parse(import.meta.env.CUSTOME_MODE_DEV) === true ? import.meta.env.CUSTOM_DEV : import.meta.env.CUSTOM_PROD,
+  api: import.meta.env.VITE_MODE_DEV === "true" ? import.meta.env.VITE_API_URL_DEV : import.meta.env.VITE_API_URL_PROD,
 };
