3d641051caf13982f389c84b4ef377921fc2bac2
Danyil <68147415+kdv1995@users.noreply.github.com>
2024-12-10 12:44:02 +0200

    enhancement: contribution interface updated

--- src/core/interfaces/contribution.interface.ts
+++ src/core/interfaces/contribution.interface.ts
@@ -1,46 +1,16 @@
 export interface Contribution {
   id: number;
   name: string;
+  contributedAt: string;
+  canceledAt: string | null;
   infraProvider: string;
+  softwareStack: string;
   region: string;
+  version: string;
   cpu: number;
   ram: number;
   diskSizeGb: number;
-  softwareStack: string;
-  version: string;
-  contributedAt: string;
-  canceledAt: string | null;
+  earnedAmount: number;
+  reward: string;
+  timeLeftToReward: number;
 }
-//canceledAt
-//:
-//null
-//contributedAt
-//:
-//"2024-12-04T15:09:14.482Z"
-//cpu
-//:
-//4
-//diskSizeGb
-//:
-//1024
-//id
-//:
-//2
-//infraProvider
-//:
-//"AWS"
-//name
-//:
-//"AWS"
-//ram
-//:
-//1024
-//region
-//:
-//"Europe_East"
-//softwareStack
-//:
-//"PostgreSQL"
-//version
-//:
-//"16"
